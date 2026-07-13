5f490dbe0c158b3d2c0ddbcd51ffe05d3f156b37
DondeLoExan <dev@dondoloexan.app>
2026-07-13 14:01:03 +0200

    Bump v2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dondeloexan"
         minSdk = 26
         targetSdk = 34
-        versionCode = 35
-        versionName = "2.0.3"
+        versionCode = 36
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
